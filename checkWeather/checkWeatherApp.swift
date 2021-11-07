eb5650dc74e9a1586e129a5f13f8df4022396561
Walter Cojal <walter_cojal@hotmail.com>
2021-11-06 19:18:37 -0500

    Initial Commit

---
+++ checkWeather/checkWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkWeatherApp.swift
+//  checkWeather
+//
+//  Created by Walter Santiago Cojal Medina on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct checkWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
